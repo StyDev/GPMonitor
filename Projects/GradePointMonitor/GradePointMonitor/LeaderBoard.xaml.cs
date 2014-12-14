23134797dd8c3a6a9bde0c3ae51357f8e62bffb0
AZEEZAT RAHEEM <azeezat94@gmail.com>
2014-12-14 21:11:53 +0100

    First commit

---
+++ Projects/GradePointMonitor/GradePointMonitor/LeaderBoard.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace GradePointMonitor
+{
+    public partial class LeaderBoard : PhoneApplicationPage
+    {
+        public LeaderBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
---
+++ Projects/GradePointMonitor/GradePointMonitor/Menu.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace GradePointMonitor
+{
+    public partial class Menu : PhoneApplicationPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void PredictYourGrade_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Alert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LeaderBoard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TimeTable_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditProfile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CourseView_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        
+    }
+}
